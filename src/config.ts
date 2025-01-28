19a367d658483dff2ddcf7a00a8107176b44839d
F1yingWhite <81723334@qq.com>
2025-01-28 14:33:30 +0800

    改个头像

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/avatar2.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'FlyingWhite',
   bio: 'Good monring my neighbors!',
   links: [
