8252b7635dadcc4b15af96e89dc1e9779da9d50b
F1yingWhite <81723334@qq.com>
2025-08-12 20:51:53 +0800

    把背景改为42

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
   },
   banner: {
     enable: true,
-    src: 'assets/images/wmremove-transformed.jpeg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/1755002954662.jpeg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
     credit: {
       enable: false,         // Display the credit text of the banner image
